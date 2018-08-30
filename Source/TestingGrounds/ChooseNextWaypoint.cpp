5ed1880edd5f80c1e26a88790876cd3f8ef31d49
Rameez Qurashi <rameez704@gmail.com>
2018-08-30 18:29:43 -0400

    Create ChooseNextWaypoint BTTask in C++

---
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ChooseNextWaypoint.h"
+
+EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Execute task called from c++"));
+
+	return EBTNodeResult::Succeeded;
+}
+
+
+
